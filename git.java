d03e1b753c9196491c740d0bd39120f94f6af18a
Manoranjan <manosakthivel2004@gmail.com>
2023-09-19 11:34:09 +0530

    Version 2

--- git.java
+++ git.java
@@ -1,6 +1,8 @@
 class main{
-    public static void main(string []args
+    public static void main(string []args)
     {
         System.out.println("Hello Git");
-    })
+        System.out.println("Hello palani");
+
+    }
 }
